ae01cc2290e1876e9e25f1b75a85b28eb377eb0a
hamilton <hamilton-souza@outlook.com.br>
2022-01-02 11:31:43 -0300

    excluded structure and teste methods

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
+// ter uma variavel que recebe o local
+// uma variavel para receber os dados
+// criar um método para pegar os dados da api
+
 const apiKey = '6ad7a6de723a46efbc5180552213112';
 
 const vm = new Vue({
   el: "#app",
   data: {
-    defaultCity: {},
-  },
-  methods: {
-    fetchInitial() {
-      fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=pelotas&days=5&aqi=no&alerts=no
-      `)
-        .then(response => response.json())
-        .then(data => {
-          this.defaultCity = data;
-        })
-    }
-  },
-  created() {
-    this.fetchInitial();
-  },
+    location: 'Contagem',
+    dataLocation: {},
+  }
 })
