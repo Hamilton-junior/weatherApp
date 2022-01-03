cd4569237457684f18c0d832042fa17c9922d5c4
hamilton <hamilton-souza@outlook.com.br>
2022-01-03 09:04:11 -0300

    created filters

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ const vm = new Vue({
     sectionDataOpen: true,
     inputValue: "",
   },
+  filters: {
+    degreesCelsius(value) {
+      return `${value}ºc`;
+    },
+    dataTransform(value) {
+      const dataValue = value.split("-");
+      return `${dataValue[2]}-${dataValue[1]}-${dataValue[0]}`
+    },
+    localTimeTransform(value) {
+      const timeValue = value.split(" ");
+      const date = timeValue[0];
+      const time = timeValue[1];
+      const dateTransform = date.split("-");
+      return `${dateTransform[2]}-${dateTransform[1]}-${dateTransform[0]} | ${time}`
+    },
+  },
   methods: {
     fetchData() {
       fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${this.location}&days=5&aqi=no&alerts=no&lang=pt`)
